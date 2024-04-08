92a1c52f1d82f55c8f82d19e9379a1fbd269d589
Chaesunbak <coanstjd0422@gmail.com>
2024-04-09 02:55:11 +0900

    index

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8HWH8qDoNOtIolsSqXUWe7MrMT-rELsI",
-  authDomain: "spotify-3e40e.firebaseapp.com",
-  projectId: "spotify-3e40e",
-  storageBucket: "spotify-3e40e.appspot.com",
-  messagingSenderId: "845695152408",
-  appId: "1:845695152408:web:63e2e9804aa0dc343c6e6d",
-  measurementId: "G-GDBJEQ9397"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMNET_ID
 };
 
 const app = initializeApp(firebaseConfig);
