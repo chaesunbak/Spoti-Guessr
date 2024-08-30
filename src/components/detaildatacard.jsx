728f98815d24543318093dd94324fb6bacd8a0c2
Chaesunbak <coanstjd0422@gmail.com>
2024-08-31 01:28:39 +0900

    feat : detaildatacard 아티스트 이름 표시 개선

--- src/components/detaildatacard.jsx
+++ src/components/detaildatacard.jsx
@@ -6,15 +6,9 @@ export default function DetailDataCard({gameData}){
     
     return (
         <div className="flex flex-col min-h-[22rem] rounded-xl bg-slate-500 text-white p-4">
-            {gameData?.id && <p>스포티파이 ID : {gameData.id}</p>}
             {gameData?.album_type && <p>앨범 유형 : {gameData.album_type}</p>}
             {gameData?.name && <p>이름 : {gameData.name}</p>}
-            {gameData?.artist1_id && <p>아티스트1 ID : {gameData.artist1_id}</p>}
-            {gameData?.artist1_name && <p>아티스트1 이름 : {gameData.artist1_name}</p>}
-            {gameData?.artist2_id && <p>아티스트2 ID : {gameData.artist2_id}</p>}
-            {gameData?.artist2_name && <p>아티스트2 이름 : {gameData.artist2_name}</p>}
-            {gameData?.artist3_id && <p>아티스트3 ID : {gameData.artist3_id}</p>}
-            {gameData?.artist3_name && <p>아티스트3 이름 : {gameData.artist3_name}</p>}
+            <p>아티스트 : {[gameData.artist1_name, gameData.artist2_name, gameData.artist3_name].filter(Boolean).join(', ')}</p>
             {gameData?.release_date && <p>발매일 : {gameData.release_date}</p>}
             {gameData?.followers && <p>팔로워 : {gameData.followers}</p>}
             {gameData?.genres && <p>장르 : {gameData.genres.join(', ')}</p>}
