6afce2fe2539a1c035504aaad4ba80951fa30b78
Chaesunbak <coanstjd0422@gmail.com>
2024-08-30 13:01:23 +0900

    style

--- src/components/detaildatacard.jsx
+++ src/components/detaildatacard.jsx
@@ -19,8 +19,8 @@ export default function DetailDataCard({gameData}){
             {gameData?.release_date && <p>발매일 : {gameData.release_date}</p>}
             {gameData?.followers && <p>팔로워 : {gameData.followers}</p>}
             {gameData?.genres && <p>장르 : {gameData.genres.join(', ')}</p>}
-            {gameData?.spotifylink && <a href={gameData.spotifylink}>스포티파이에서 열기</a>}
             {gameData?.updatedAt && <p>업데이트 일자 : {formatDate(gameData.updatedAt)}</p>}
+            {gameData?.spotifylink && <a className="underline"href={gameData.spotifylink}>스포티파이에서 열기</a>}
         </div>
     )
 }
--- src/pages/Data/DataList/SingleData/singledata.jsx
+++ src/pages/Data/DataList/SingleData/singledata.jsx
@@ -19,30 +19,6 @@ const getGameModeTitle = (mode) => {
     }
 };
 
-const fetchData = async (gamemode, id, setData, setLoading, navigate) => {
-    setLoading(true);
-    const docRef = doc(db, gamemode, id);
-
-    try {
-        const docSnap = await getDoc(docRef);
-
-        if (docSnap.exists()) {
-            console.log("Document data:", docSnap.data());
-            setData(docSnap.data());
-        } else {
-            console.log("No such document!");
-
-            if (window.confirm("문서가 존재하지 않습니다. 이전 페이지로 돌아가시겠습니까?")) {
-                navigate(-1);
-            }
-        }
-    } catch (e) {
-        console.error("Error getting document:", e);
-    } finally {
-        setLoading(false);
-    }
-};
-
 export default function SingleData() {
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState();
@@ -53,6 +29,31 @@ export default function SingleData() {
     const id = params.id;
 
     useEffect(() => {
+
+        const fetchData = async (gamemode, id, setData, setLoading, navigate) => {
+            setLoading(true);
+            const docRef = doc(db, gamemode, id);
+        
+            try {
+                const docSnap = await getDoc(docRef);
+        
+                if (docSnap.exists()) {
+                    console.log("Document data:", docSnap.data());
+                    setData(docSnap.data());
+                } else {
+                    console.log("No such document!");
+        
+                    if (window.confirm("문서가 존재하지 않습니다. 이전 페이지로 돌아가시겠습니까?")) {
+                        navigate(-1);
+                    }
+                }
+            } catch (e) {
+                console.error("Error getting document:", e);
+            } finally {
+                setLoading(false);
+            }
+        };
+
         fetchData(gamemode, id, setData, setLoading, navigate);
     }, [gamemode, id, navigate]);
 
@@ -74,7 +75,7 @@ export default function SingleData() {
             </h2>
             <div className="flex flex-col md:grid md:grid-cols-3">
                 <PlayCard gameData={data} />
-                <div className="md:ml-4 md:col-span-2">
+                <div className="mt-2 md:mt-0 md:ml-4 md:col-span-2">
                     <DetailDataCard gameData={data} />
                 </div>
             </div>
