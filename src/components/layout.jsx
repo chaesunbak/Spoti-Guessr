1c751984ea4748a520184785189e264bdde81a03
Chaesunbak <coanstjd0422@gmail.com>
2024-09-04 23:32:00 +0900

    chore : ESlint와 프리티어 적용

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,34 +1,35 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
-import { ResizableHandle, ResizablePanel, ResizablePanelGroup} from "@/components/ui/resizable"
-import { ScrollArea } from "@/components/ui/scroll-area"
-import SideBar from "./sidebar";
-import { Toaster } from "@/components/ui/toaster"
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import SideBar from './sidebar';
+import { Toaster } from '@/components/ui/toaster';
 import MobileMenu from './mobilemenu';
 import UserIcon from './UserIcon/usericon';
-
 
 function Layout() {
   const isMobile = useMediaQuery({ maxWidth: 767 });
 
-  if(isMobile)
-  return (
-    <>
-      <div className="flex justify-between">
-        <Link to="/"><h1 className="font-sans text-2xl font-bold m-1">Spoti Guesser</h1></Link>
-        <UserIcon/>
-        <MobileMenu/>
-      </div>
-      <Outlet/>
-      <Toaster />
-    </>
-  )
+  if (isMobile)
+    return (
+      <>
+        <div className="flex justify-between">
+          <Link to="/">
+            <h1 className="font-sans text-2xl font-bold m-1">Spoti Guesser</h1>
+          </Link>
+          <UserIcon />
+          <MobileMenu />
+        </div>
+        <Outlet />
+        <Toaster />
+      </>
+    );
 
   if (!isMobile)
-  return (
+    return (
       <ResizablePanelGroup autoSaveId="persistence" direction="horizontal" className="size-full">
         <ResizablePanel defaultSize={20} minSize={5} className="h-full">
-           <SideBar className="h-full p-4" />
+          <SideBar className="h-full p-4" />
         </ResizablePanel>
         <ResizableHandle withHandle />
         <ResizablePanel defaultSize={80} minSize={70}>
@@ -38,7 +39,7 @@ function Layout() {
           <Toaster />
         </ResizablePanel>
       </ResizablePanelGroup>
-  )
+    );
 }
 
-export default Layout
+export default Layout;
