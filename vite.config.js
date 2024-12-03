97d25b38c04b86e1ca0fc7f0bcb96eee95ebf1d6
Chaesunbak <coanstjd0422@gmail.com>
2024-12-03 16:04:58 +0900

    fix : vite config 수정

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/',
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx'], // 파일 확장자 해결을 위해 추가
+  },
+  build: {
+    rollupOptions: {
+      // 빌드 시 파일 경로 해결을 위한 설정
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
     },
   },
-})
+});
